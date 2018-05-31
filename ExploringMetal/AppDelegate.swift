4ff852a1c2dc2005ff5ce83ef45b1ded7a54de9c
Adil Patel <adilpatel@me.com>
2018-05-31 12:36:24 +0200

    Initial Commit

---
+++ ExploringMetal/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ExploringMetal
+//
+//  Created by Adil Patel on 31/05/2018.
+//  Copyright © 2018 Adil Patel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
